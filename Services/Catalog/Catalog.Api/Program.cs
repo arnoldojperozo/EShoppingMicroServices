54fc8088d2dbeec7735ac49938e212eba59f1e9f
Arnoldo Perozo <arnoldojperozo@gmail.com>
2023-07-23 19:39:25 -0400

    Catalog Api - Project Startup, Package configuration

--- Services/Catalog/Catalog.Api/Program.cs
+++ Services/Catalog/Catalog.Api/Program.cs
@@ -1,3 +1,12 @@
+using System.Reflection;
+using Catalog.Application.Handlers.Commands;
+using Catalog.Core.Repository.Interfaces;
+using Catalog.Infrastructure.Data;
+using Catalog.Infrastructure.Data.Interfaces;
+using Catalog.Infrastructure.Repositories;
+using MediatR;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +14,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApiVersioning();
+builder.Services.AddHealthChecks()
+    .AddMongoDb(builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionString"), "Catalog Mongo Db Health Check",
+        HealthStatus.Degraded);
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddMediatR(typeof(CreateProductCommandHandler).GetTypeInfo().Assembly);
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductBrandRepository, ProductRepository>();
+builder.Services.AddScoped<IProductTypeRepository, ProductRepository>();
+
 
 var app = builder.Build();
 
