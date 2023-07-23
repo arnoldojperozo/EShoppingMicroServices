2ab26295afbbad82d17f86b1fa79a59ffb077ba1
Arnoldo Perozo <arnoldojperozo@gmail.com>
2023-07-23 19:13:20 -0400

    Catalog Api - Base Controller

---
+++ Services/Catalog/Catalog.Api/Controllers/ApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
+
+[ApiVersion("1")]
+[Route("api/v{version:apiversion}/[controller])"]
+public class ApiController : ControllerBase
+{
+    
+}
