00e15edc213b3bd9fed6da7f52d393ef1028687d
Arnoldo Perozo <arnoldojperozo@gmail.com>
2023-07-23 19:52:31 -0400

    Catalog Api - Catalog Controller Methods

--- Services/Catalog/Catalog.Api/Controllers/CatalogController.cs
+++ Services/Catalog/Catalog.Api/Controllers/CatalogController.cs
@@ -1,8 +1,31 @@
-﻿namespace Catalog.Api.Controllers;
+﻿using System.Net;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
 
 public class CatalogController : ApiController
 {
+    private readonly IMediator _mediator;
+
+    public CatalogController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
     
-    
-    
+    #region GET Methods
+    [HttpGet]
+    [Route("[action]/id", Name = "GetProductById")]
+    [ProducesResponseType(typeof(ProductResponse),(int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ProductResponse>> GetProductById(string id)
+    {
+        var query = new GetProductByIdQuery(id);
+
+        var result = await _mediator.Send(query);
+
+        return Ok(result);
+    }
+    #endregion
 }
