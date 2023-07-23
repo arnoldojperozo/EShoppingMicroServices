67f9324389c1f2b98550520bf055e1e82aec9d09
Arnoldo Perozo <arnoldojperozo@gmail.com>
2023-07-23 14:31:33 -0400

    Catalog Application - Product Types Handler

---
+++ Services/Catalog/Catalog.Application/Handlers/GetAllTypesHandler.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repository.Interfaces;
+using MediatR;
+
+namespace Catalog.Application.Handlers;
+
+public class GetAllTypesHandler : IRequestHandler<GetAllTypesQuery, IList<TypesResponse>>
+{
+    private readonly IProductTypeRepository _typesRepository;
+
+    public GetAllTypesHandler(IProductTypeRepository typesRepository)
+    {
+        _typesRepository = typesRepository;
+    }
+    
+    public async Task<IList<TypesResponse>> Handle(GetAllTypesQuery request, CancellationToken cancellationToken)
+    {
+        var typesList = await _typesRepository.GetAllProductTypes();
+
+        var typesResponseList = ProductMapper.Mapper.Map<IList<TypesResponse>>(typesList);
+
+        return typesResponseList;
+    }
+}
