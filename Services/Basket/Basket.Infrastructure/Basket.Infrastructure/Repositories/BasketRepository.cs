579e0cade2d00117f56dc31aebb67fcabd4c831f
Arnoldo Perozo <arnoldojperozo@gmail.com>
2023-08-08 19:40:45 -0400

    Service Basket - Repository

---
+++ Services/Basket/Basket.Infrastructure/Basket.Infrastructure/Repositories/BasketRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using Basket.Core.Entities;
+using Basket.Core.Repositories.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Basket.Infrastructure.Repositories
+{
+    internal class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache;
+        }
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+
+            return JsonSerializer.Deserialize<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart)
+        {
+            await _redisCache.SetStringAsync(shoppingCart.UserName, JsonSerializer.Serialize(shoppingCart));
+
+            return await GetBasket(shoppingCart.UserName);
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+    }
+}
