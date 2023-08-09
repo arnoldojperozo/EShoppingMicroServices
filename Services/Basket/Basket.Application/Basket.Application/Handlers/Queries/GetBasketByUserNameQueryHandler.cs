6c3de4daa1781570cc45776f218df276d3fc6b0e
Arnoldo Perozo <arnoldojperozo@gmail.com>
2023-08-08 20:46:39 -0400

    Basket Service - Get BasketQuery

---
+++ Services/Basket/Basket.Application/Basket.Application/Handlers/Queries/GetBasketByUserNameQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Basket.Application.Mappers;
+using Basket.Core.Repositories.Interfaces;
+using MediatR;
+
+namespace Basket.Application.Handlers.Queries;
+public class GetBasketByUserNameQueryHandler : IRequestHandler<GetBasketByUserNameQuery, ShoppingCartResponse>
+{
+    private readonly IBasketRepository _basketRepository;
+
+    public GetBasketByUserNameQueryHandler(IBasketRepository basketRepository)
+    {
+        _basketRepository=basketRepository;
+    }
+
+    public async Task<ShoppingCartResponse> Handle(GetBasketByUserNameQuery request, CancellationToken cancellationToken)
+    {
+        var shoppingCart = await _basketRepository.GetBasket(request.UserName);
+
+        var shoppingCartResponse=BasketMapper.Mapper.Map<ShoppingCartResponse>(shoppingCart);
+
+        return shoppingCartResponse;
+    }
+}
---
+++ Services/Basket/Basket.Application/Basket.Application/Queries/GetBasketByUserNameQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Basket.Application.Queries
+
+public class GetBasketByUserNameQuery : IRequest<ShoppingCartResponse>
+{
+    public string UserName { get; set; }
+
+    public GetBasketByUserNameQuery(string userName)
+    {
+        UserName = userName;
+    }
+}
