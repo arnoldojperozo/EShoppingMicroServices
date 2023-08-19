e3834050141715ee965f22ea8eeb66286470380f
Arnoldo Perozo <arnoldojperozo@gmail.com>
2023-08-19 11:34:56 -0400

    Basket Service - Basket Controller

---
+++ Services/Basket/Basket.Api/Controllers/APIController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Api.Controllers;
+
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class ApiController : ControllerBase
+{
+
+}
