2f39675623d0db83a960741411f8d4a958cbd284
Arnoldo J. Perozo Mena <arnoldo.perozo@hotmail.com>
2023-11-25 08:46:04 -0500

    Discount Service - Uodate Discount

--- Services/Discount/Discount.Application/Discount.Application/Commands/UpdateDiscountCommand.cs
+++ Services/Discount/Discount.Application/Discount.Application/Commands/UpdateDiscountCommand.cs
@@ -3,8 +3,9 @@ using MediatR;
 
 namespace Discount.Application.Commands;
 
-public class CreateDiscountCommand : IRequest<CouponModel>
+public class UpdateDiscountCommand : IRequest<CouponModel>
 {
+    public int Id { get; set; }
     public string ProductName { get; set; }
     public string Amount { get; set; }
     public string Description { get; set; }
